8a0093d2f0b0ae774bad43640d03e3a8d3bba99e
Max Maleno <mmaleno@hmc.edu>
2019-06-18 00:42:06 -0700

    c code now splits up each time division into its digits

--- c_time_test/c_time.c
+++ c_time_test/c_time.c
@@ -15,8 +15,6 @@ int * getRemainingTime()
     minutes = ((diffUnixTime % (60*60*24)) % (60*60)) / 60;
     seconds = (((diffUnixTime % (60*60*24)) % (60*60)) % 60);
 
-    printf("TEST!! Unix Difference: %lu ---- ", diffUnixTime);
-
     static int times[5];
     times[0] = days;
     times[1] = hours;
@@ -27,19 +25,50 @@ int * getRemainingTime()
     return times;
 }
 
+int * getDigits(int * times)
+{
+    int dayHundreds, dayTens, dayOnes, hourTens, hourOnes,
+        minutesTens, minutesOnes, secondsTens, secondsOnes;
+    
+    static int digits[9]; // 3 days, 2 hours, 2 minutes, 2 seconds
 
+    digits[0] = times[0] / 100;
+
+    for(int i = 0; i<6; i++)
+    {
+        digits[2*i+1] = times[i] / 10;
+        digits[2*i+2] = times[i] % 10;
+    }
+
+    return digits;
+}
+
+void delay()
+{
+    int c, d;
+    
+    for (c = 1; c <= 10000; c++) for (d = 1; d <= 10000; d++) {}
+
+}
+
+// TODO: make function that takes an int and outputs array of segment on/offs
 
 int main()
 {
     int * times;
+    int * digits;
     while(1)
     {
         times = getRemainingTime();
+        digits = getDigits(times);
+        delay();                        // for readablity
+        /*
         printf("Unix Difference: %d, ", times[4]);
-        printf("Days: %d, ", times[0]);
-        printf("Hours: %d, ", times[1]);
-        printf("Minutes: %d, ", times[2]);
-        printf("Seconds: %d\n", times[3]);
+        printf("Days: %d (hundreds %d - tens %d - ones %d) ; ", times[0], digits[0], digits[1], digits[2]);
+        printf("Hours: %d, (tens %d - ones %d) ; ", times[1], digits[3], digits[4]);
+        printf("Minutes: %d, (tens %d - ones %d) ; ", times[2], digits[5], digits[6]);
+        printf("Seconds: %d (tens %d - ones %d) ; \n", times[3], digits[7], digits[8]);
+        */
     }
     return 0;
 }
---
+++ c_time_test/testing_file.c
@@ -0,0 +1,18 @@
+// file to test things in
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    
+int big = 360;
+
+int quotient = 399 / 100;
+
+printf("%d\n", quotient);
+
+return 0;
+
+}
